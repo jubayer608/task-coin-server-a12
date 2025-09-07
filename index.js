7490320af1e3f03f8b00fc0c1e10c631f38e632e
jubayer608 <sheikhjubayer63@gmail.com>
2025-09-07 19:42:52 +0600

    secure firebase admin key

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ admin.initializeApp({
 });
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akopiuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
